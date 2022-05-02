eae12ddc791ff467294c47968f2785a76b5f3881
unknown <ericbwebdev86@gmail.com>
2022-05-02 11:27:20 -0500

    thread schema

---
+++ server/models/Thread.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+const Reply = require('./Reply');
+
+
+const threadSchema = new Schema({
+  threadTitle: {
+    type: String,
+    required: true,
+  },
+  threadContent: {
+      type: String,
+      required: true,
+  },
+  threadImage: {
+      type: String,
+  },
+  replies: [Reply.schema],
+  replyCount: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Reply'
+    }
+  ]
+});
+
+// set up pre-save middleware to create password
+
+
+const Thread = mongoose.model('Thread', threadSchema);
+
+module.exports = Thread;
