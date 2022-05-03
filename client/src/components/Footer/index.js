51ed84e1c2478cf388497ea18fd291ea32387760
Matt Kolbach <mattkolbach@yahoo.com>
2022-05-03 17:18:22 -0500

    basic footer layout complete

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,11 +3,12 @@ import React from 'react';
 const Footer = () => {
     
   return (
-    <footer className="container-lg flex-row justify-content-center">
-          <div className="d-flex align-items-center">
-            &copy;2022 by 4 Guys
-            </div>          
-          <div className="d-flex align-items-center">
+    <footer className="container-lg flex-row">
+          <div className="d-flex align-items-center justify-content-center">
+            <div className='mx-3 ms-auto'>
+              &copy;2022 by 4 Guys
+            </div>         
+          <div className="d-flex align-items-center mx-3 ms-auto">
             <div className="col-md">
               <h6>Contact Us</h6>
               <a href="https://ericbwebdev86.github.io/abn-dev-portfolio/" target="_blank" rel="noreferrer noopener">Eric Bates</a><br />
@@ -15,7 +16,7 @@ const Footer = () => {
               <a href="https://mattkolbach.github.io/mattkportfolio/" target="_blank" rel="noreferrer noopener">Matt Kolbach</a><br />
               <a href="#" target="_blank" rel="noreferrer noopener">Ryan Thiel</a>
             </div>
-          </div>
+          </div></div> 
     </footer>
   );
 };
