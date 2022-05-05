34a25488d8309d8c4737710bb2669550b7919362
Preston Fassbender <fassbenderp0551@gmail.com>
2022-05-04 20:01:06 -0500

    finished login form functionality

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,26 +1,72 @@
 import React, { useState } from 'react'
 
+import { useMutation } from '@apollo/client';
+import { Link } from 'react-router-dom';
+import { LOGIN } from '../../utils/mutations';
+
+import Auth from '../../utils/auth.js'
+
 const LoginForm = () => {
     const [formState, setFormState] = useState({
         username: '',
         password: ''
     });
+    const [login, { error }] = useMutation(LOGIN);
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        console.log("form submitted")
+        try {
+            const mutationResponse = await login({
+                variables: { email: formState.username, password: formState.password },
+            });
+            const token = mutationResponse.data.login.token;
+            Auth.login(token);
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
 
     return (
         <div className='container'>
-            <h1>
-                LOGIN FOR THE FULL EXPERIENCE, BITCH!
-            </h1>
-            <form>
+            <form onSubmit={handleFormSubmit}>
                 <div className='form-group'>
                     <label for='usernameInput'>Username</label>
-                    <input type={'text'} className='form-control' id='usernameInput' placeholder='Enter Username'></input>
+                    <input
+                        type={'text'}
+                        className='form-control'
+                        id='usernameInput'
+                        name='username'
+                        placeholder='Enter Username'
+                        onChange={handleChange}
+                    />
                 </div>
                 <div className='form-group'>
                     <label for='passwordInput'>Password</label>
-                    <input type={'password'} className='form-control' id='passwordInput' placeholder='********'></input>
+                    <input
+                        type={'password'}
+                        className='form-control'
+                        id='passwordInput'
+                        name='password'
+                        placeholder='********'
+                        onChange={handleChange}
+                    />
                 </div>
+                {error ? (
+                    <div>
+                        <p className="error-text">The provided credentials are incorrect</p>
+                    </div>
+                ) : null}
                 <button type={'submit'} className='btn btn-primary'>Submit</button>
+                <Link to="/signup">← Don't have an account? Sign up now!</Link>
             </form>
         </div>
     )
