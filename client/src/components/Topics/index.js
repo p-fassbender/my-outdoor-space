8e4acbd91e7f68eab73266fdb71bae0f4c262f5b
Matt Kolbach <mattkolbach@yahoo.com>
2022-05-04 20:54:50 -0500

    hardcoded home page, general layout

--- client/src/components/Topics/index.js
+++ client/src/components/Topics/index.js
@@ -5,9 +5,14 @@ const Topics = () => {
 
     
   return (
-    <div className="container">
-
+    <div className="d-flex topBorder">
+    <div className="col-2 cardColOne">
+      <p className="m-3">Icon</p>
     </div>
+    <div className="col-10 d-flex align-items-center">
+      <Link to="/Dynamic" className="m-2">Dynamic</Link>
+    </div>
+  </div>
   );
 };
 
